50e742fbcee1495b4311462598a506fa362084a5
jswiss <swissjoshua@gmail.com>
2020-06-17 12:52:41 +0100

    reverted models

--- android/app/src/main/java/com/bitikoo/MainApplication.java
+++ android/app/src/main/java/com/bitikoo/MainApplication.java
@@ -10,8 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.nozbe.watermelondb.WatermelonDBPackage;
-
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          packages.add(new WatermelonDBPackage());
+          // packages.add(new MyReactNativePackage());
           return packages;
         }
 
--- components/App/App.test.tsx
+++ components/App/App.test.tsx
@@ -1,3 +1,7 @@
+/**
+ * @format
+ */
+
 import 'react-native';
 import React from 'react';
 import { render } from '@testing-library/react-native';
