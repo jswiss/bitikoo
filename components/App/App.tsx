6084e546928b2976d8516f05dce6ef5d0d2e59ec
jswiss <swissjoshua@gmail.com>
2020-06-17 10:55:34 +0100

    watermelondb setup for ios and android

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,73 +1,35 @@
 // rn gesture handler needed, as per reactnavigation docs: https://reactnavigation.org/docs/getting-started
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer, ParamListBase } from '@react-navigation/native';
-import {
-  createStackNavigator,
-  StackScreenProps,
-} from '@react-navigation/stack';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen({ navigation }: StackScreenProps<ParamListBase>) {
-  console.log(navigation);
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Overview"
-        onPress={() =>
-          navigation.navigate('Overview', {
-            itemId: 21,
-            otherParam: 'boop',
-          })
-        }
-      />
-    </View>
-  );
-}
-
-function Overview({ navigation }: StackScreenProps<ParamListBase>) {
+function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Overview Screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Text>Home!</Text>
     </View>
   );
 }
 
-function ManageMyShops() {
+function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Manage My Shops</Text>
-    </View>
-  );
-}
-function AddShop() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Add a Shop</Text>
+      <Text>Settings!</Text>
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Overview" component={Overview} />
-        <Stack.Screen name="Manage My Shops" component={ManageMyShops} />
-        <Stack.Screen name="Add a Shop" component={AddShop} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  bitikoo
+//
+//  Created by Joshua Swiss on 17/06/2020.
+//
+
+import Foundation
